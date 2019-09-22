692a3e77343619d6ae3267a848c1bfeba7d3db6f
jchen303 <54388596+jchen303@users.noreply.github.com>
2019-09-22 16:08:35 -0700

    Create brothersbeforepromiscuouswomen.java
    8/26/19 ascii art

---
+++ code/bpj/brothersbeforepromiscuouswomen.java
@@ -0,0 +1,14 @@
+public class brothersbeforepromiscuouswomen {
+         public static void main(String[] args) {
+             System.out.println("  (---- <3    <3----          ----");
+             System.out.println(" //| . .|\\<3<3 |. . |        |. . | ");
+             System.out.println("// |  ) | \\  <3| <  |        | 0  |");
+             System.out.println("    ----  <3    ----          ----");
+             System.out.println("      |<3   <3 <3|             |");
+             System.out.println(" 0----|----00----|----0   0----|----0   ");
+             System.out.println("      |          |             |");
+             System.out.println("     / \\        / \\           / \\");
+             System.out.println("    /   \\      /   \\         /   \\");
+             String j = "\t\t\t third wheel :(";
+             System.out.println(j);}  
+            }
