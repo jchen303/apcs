929ef405e3a2e78c122cb943b0b00752ed38f059
jchen303 <54388596+jchen303@users.noreply.github.com>
2019-09-22 16:19:49 -0700

    Create HelloWorldApp.java
    8/26/19 hello, world

---
+++ code/helloworld/HelloWorldApp.java
@@ -0,0 +1,17 @@
+/*
+ To change this template, choose Tools | Templates
+ and open the template in the editor.
+ */
+package HelloWorldApp;
+/**
+ The HelloWorldApp class implements an application that
+ simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp {
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String []args) {
+		System.out.println("Hello World!"); // Display the string.
+	}
+}
