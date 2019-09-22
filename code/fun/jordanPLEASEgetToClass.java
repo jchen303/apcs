b91025d928869ef1e59683e0de234022eb766134
jchen303 <54388596+jchen303@users.noreply.github.com>
2019-09-22 16:23:49 -0700

    Create jordanPLEASEgetToClass.java
    9/4/19 jordan please get to class.

---
+++ code/fun/jordanPLEASEgetToClass.java
@@ -0,0 +1,27 @@
+public class jordanPLEASEgetToClass
+{
+    public static void main(String[] args)
+    {
+        int jordan = 0;
+        while (jordan < 69000)
+        {
+            if (jordan / 3 == 0)
+            {
+                System.out.print("              jordan get to class please.              ");
+                try
+                {
+                    Thread.sleep(6);
+                } catch (InterruptedException e) {}
+            }
+            if (jordan / 2 == 0)
+            {
+                System.out.print("jordan PLEASE get to class.                                             ");
+                try
+                {
+                    Thread.sleep(23);
+                } catch (InterruptedException f) {}
+            }
+            
+        }
+    }
+}
