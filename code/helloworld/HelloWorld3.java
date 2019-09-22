8ae06843d722efa90cfd1069ad1fe73ce1780db1
jchen303 <54388596+jchen303@users.noreply.github.com>
2019-09-22 16:24:52 -0700

    Create HelloWorld3.java
    8/25/19 bruh fist bumping a bruh (terminal width must be 80 columns)

---
+++ code/helloworld/HelloWorld3.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HelloWorld3;
+
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "this shit doesn't even matter does it" to standard output.
+ */
+ public class HelloWorld3 {
+
+
+        /**
+         * @param args the command line arguments
+         */
+         public static void main(String[] args) {
+             System.out.println(
+             "  ----     ----   bruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruhbr"
+             +
+             " | . .|   |. . |  uhbruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruh"
+             +
+             " |  ) |   | (  |  bruhbruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabr"
+             +
+             "  ----     ----   uhbruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruh"
+             +
+             "    |        |    bruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruhbr"
+             +
+             " ---|---00---|--- uhbruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruh"
+             +
+             "    |        |    bruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruhbr"
+             +
+             "   / \\      / \\   uhbruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruh"
+             +
+             "  /   \\    /   \\  bruhfistbumpingabruhbruhfistbumpingabruhbruhfistbumpingabruhbr"
+
+             ); // Display the string.
+         }
+ }
