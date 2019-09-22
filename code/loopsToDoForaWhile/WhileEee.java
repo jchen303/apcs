feeb28c878cef5b3ed0fbfe1e1a71bb9740e48fd
jchen303 <54388596+jchen303@users.noreply.github.com>
2019-09-22 16:17:39 -0700

    Create WhileEee.java
    9/20/19 while loops

---
+++ code/loopsToDoForaWhile/WhileEee.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class WhileEee
+{
+    public static void main(String[] args)
+    {
+        Scanner ooo = new Scanner(System.in);
+        System.out.println("please enter an integer");
+        int oooo = ooo.nextInt();
+        int ahhAhhhAhhhhhhh = 1;
+
+        System.out.print(oooo + " has divisors ");
+
+        while (ahhAhhhAhhhhhhh < oooo)
+        {
+            if (oooo % ahhAhhhAhhhhhhh == 0)
+                System.out.print(ahhAhhhAhhhhhhh + " ");
+            ahhAhhhAhhhhhhh++;
+        }
+        System.out.println();
+    }
+}
